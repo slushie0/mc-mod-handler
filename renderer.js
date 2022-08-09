4627f46df676558aa3135f5ae5bd0c5296cdce09
Slushie <57730247+slushie0@users.noreply.github.com>
2022-08-08 22:51:25 -0700

    i-have-a-headache

--- renderer.js
+++ renderer.js
@@ -12,44 +12,70 @@ ipcRenderer.on('asynchronous-message', function (evt, message) {
   loadData();
 });
 
-var allModSlugs = [];
+var modList = [];
 
+/*
 function loadData() {
 fs.readFile(`${appDataPath}/.minecraft/mc-mod-handler.txt`, 'utf8', (err, data) => {
       
   // Display the file content
   try {
-  allModSlugs = JSON.parse(data);
-  for (let i in allModSlugs) {
-    addListItem(allModSlugs[i]);
-  }
-} catch (err) { return console.log(err)}
+  modList = JSON.parse(data);
+  Object.keys(modList).forEach((key, index) => {
+    addMod(key);
+  });
+  } catch (err) { return console.log(err)}
 });
 };
 
 function saveData() {
-  fs.writeFile(`${appDataPath}/.minecraft/mc-mod-handler.txt`, JSON.stringify(allModSlugs), err => { if (err) return console.error(err) });
+  fs.writeFile(`${appDataPath}/.minecraft/mc-mod-handler.txt`, JSON.stringify(modList), err => { if (err) return console.error(err) });
 };
 
-function addListItem(title) {
-  let modList = document.getElementById('mod-list');
-  let item = document.createElement('li');
-  item.classList.add('mod-item');
-  item.innerText = title;
-  modList.appendChild(item);
+*/
+function addMod(slug) {
+  slug = slug || document.getElementById('mod-slug').value;
+  if (modList.hasOwnProperty(slug)) return warn('This mod has already been added');
+  modList[slug] = {
+    title: 'Unknown'
+  };
+
+  try {
+    axios.get(`https://api.modrinth.com/v2/project/${slug}`).then(res => {
+      // dom manipulation
+      let modListEl = document.getElementById('mod-list');
+      let item = document.createElement('li');
+      let rmBtn = document.createElement('button');
+      let disableMod = document.createElement('input');
+      rmBtn.innerText = 'remove';
+      rmBtn.setAttribute('onclick', `removeMod('${slug}')`)
+      disableMod.type = 'checkbox';
+      disableMod.checked = true;
+      item.id = slug;
+      item.classList.add('mod-item');
+      item.innerText = res.data.title;
+      item.prepend(disableMod);
+      item.appendChild(rmBtn);
+      modListEl.appendChild(item);
+      
+      modList[slug] = {
+        title: res.data.title
+      };
+      saveData();
+    }).catch(function(err) { 
+      if (err.response.status == 404) return warn('Invalid Slug')
+      return warn('Something went wrong')
+    })
+  } catch (err) { return warn('Something went wrong') }
 };
 
-function addMod() {
-  let slug = document.getElementById('mod-slug').value;
+function removeMod(slug) {
+  delete modList[slug];
+  let item = document.getElementById('sodium');
+  item.remove();
+}
 
-  axios.get(`https://api.modrinth.com/v2/project/${slug}`).then(res => {
-    addListItem(res.data.title);
-    allModSlugs.push(slug);
-    saveData();
-  });
-};
-
-function downloadAllMods() {
+function downloadMods() {
   fs.rmSync(downloadsPath+'/MC Mod Folder', { recursive: true, force: true });
   for (let i in allModSlugs) {
     axios.get(`https://api.modrinth.com/v2/project/${allModSlugs[i]}/version`).then(res2 => {
@@ -58,6 +84,11 @@ function downloadAllMods() {
     });
   }
 };
+
+function warn(msg) {
+  var warnEl = document.getElementById('warning');
+  warnEl.innerText = msg;
+}
 
 ipcRenderer.on('download-success', (event, arg) => {
   console.log(arg)
