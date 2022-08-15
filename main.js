f87d538eab7232898d07a70d1b7386eb000e7d91
Slushie <57730247+slushie0@users.noreply.github.com>
2022-08-15 10:06:46 -0700

    ui update

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ ipcMain.on('download-item', async (event, urls) => {
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 1000,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
